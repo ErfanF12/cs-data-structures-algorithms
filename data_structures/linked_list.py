b9cad98b262473f908f7d3c25e7c93d741086b8f
ErfanF12 <erfan.fakour18@gmail.com>
2025-12-25 16:09:22 +0400

    Add linked list implementation

---
+++ data_structures/linked_list.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def search(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
